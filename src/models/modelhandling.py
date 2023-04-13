ad53d727a86a20c2b08ded4c10796b82af8c86ea
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-04-13 21:14:04 +0200

    changed config, smaller datasets

--- src/models/modelhandling.py
+++ src/models/modelhandling.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
 
 
