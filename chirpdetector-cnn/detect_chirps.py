fcd6f8f9e81460d350caecca36360ae0440d011a
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-04-27 17:37:07 +0200

    more plotting

--- chirpdetector-cnn/detect_chirps.py
+++ chirpdetector-cnn/detect_chirps.py
@@ -410,37 +410,37 @@ class Detector:
             chirps.extend(chunk_chirps)
 
             # plot
-            if len(chunk_chirps) > 0:
-                fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-                specshow(
-                    spec.cpu().numpy(),
-                    spec_times,
-                    spec_freqs,
-                    ax,
-                    aspect="auto",
-                    origin="lower",
+            # if len(chunk_chirps) > 0:
+            fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+            specshow(
+                spec.cpu().numpy(),
+                spec_times,
+                spec_freqs,
+                ax,
+                aspect="auto",
+                origin="lower",
+            )
+            for chirp in chunk_chirps:
+                ax.scatter(
+                    chirp[0],
+                    chirp[1],
+                    facecolors="white",
+                    edgecolors="black",
+                    s=30,
                 )
-                for chirp in chunk_chirps:
-                    ax.scatter(
-                        chirp[0],
-                        chirp[1],
-                        facecolors="white",
-                        edgecolors="black",
-                        s=30,
-                    )
-                    ax.text(
-                        chirp[0] + 0.1,
-                        chirp[1] + 30,
-                        np.round(chirp[2], 2),
-                        fontsize=10,
-                        color="white",
-                    )
-                ax.set_ylim(0, 1200)
-                plt.savefig(f"{conf.testing_data_path}/chirp_detection_{i}.png")
-                plt.cla()
-                plt.clf()
-                plt.close("all")
-                plt.close(fig)
+                ax.text(
+                    chirp[0] + 0.1,
+                    chirp[1] + 30,
+                    np.round(chirp[2], 2),
+                    fontsize=10,
+                    color="white",
+                )
+            ax.set_ylim(0, 1200)
+            plt.savefig(f"{conf.testing_data_path}/chirp_detection_{i}.png")
+            plt.cla()
+            plt.clf()
+            plt.close("all")
+            plt.close(fig)
 
             del detection_data
             del spec
