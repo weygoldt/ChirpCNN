18424f541d8384172fbbf2e7f654ddc4a6efe214
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-05-14 20:11:41 +0200

    added chirpcounter

---
+++ chirpdetector-cnn/count_on_server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Count chirps in recordings on the server. Just to know how much we have now.
+"""
+
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from detect_chirps import main as chirpdetector
+from rich import print
+from utils.logger import make_logger
+
+logger = make_logger(__name__)
+
+
+def main():
+    datapath = pathlib.Path("../data/")
+    meta = pd.read_csv(datapath / "order_meta.csv")
+
+    # only use the ones with 3 or more recordings
+    meta = meta[meta.group > 2]
+
+    # only use the ones where id1 or id2 is not nan
+    meta = meta[~meta.rec_id1.isna() | ~meta.rec_id2.isna()]
+
+    # extract the paths to the recordings
+    # and strip the FUCKING QUOTATION MARKS
+    recs = [datapath / p.strip("“”") for p in meta.recording.values]
+
+    counter = 0
+    for i, rec in enumerate(recs):
+        logger.info(f"Processing recording {i+1}/{len(recs)+1}")
+        chirps = np.load(rec / "chirp_times_cnn.npy")
+        counter += len(chirps)
+
+    print(f"We have {counter} chirps now!")
+
+
+if __name__ == "__main__":
+    main()
